30d1cd059f4ac1f2fd5ef5ab1ff197642aa576d2
Atulya <atulyakumar97@gmail.com>
2018-08-31 00:46:55 +0530

    Support Vector Machines

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1; %computed using commented code
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -24,10 +24,21 @@ sigma = 0.3;
 %
 
 
+%C= [0.01 0.03 0.1 0.3 1 3 10 30]
+%sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+% for i=1:size(C,2)
+%     for j=1:size(sigma,2)
+%  model= svmTrain(X, y, C(i), @(x1, x2) gaussianKernel(x1, x2, sigma(j)));
+%  predictions = svmPredict(model, Xval);
+%  error=mean(double(predictions ~= yval))
+%  disp(sigma(j))
+%  disp(C(i))
+%     end
+% end
 
-
-
-
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+predictions = svmPredict(model, Xval);
+error=mean(double(predictions ~= yval));
 
 % =========================================================================
 
